7cda5a1a8efb966f3021427d380fbb703f41dc98
david retzlaff <davids.retzlaff@gmail.com>
2020-08-26 07:45:07 -0300

    exercise total car example using partial application, map and reduce

--- tools/lib.js
+++ tools/lib.js
@@ -24,7 +24,7 @@
         }
         
         /* 
-            partial application
+            PARTIAL APPLICATION
             
             in a simple way, you have a function that has many arguments but you do not have all the arguments
             in moment, then you feed the function with arguments you have in the moment and then the rest passes.
@@ -33,5 +33,44 @@
         export function partialApplication(fn, ...args){
             return function(...newArgs){
                 return fn(...args,...newArgs)
+            }
+        }
+
+        /* 
+            MAP FUNCTION  map(fn)([])    
+            adapting map function
+         */
+        export function map(fn){
+            return function(arr){
+                return arr.map(fn);
+            }
+        }
+
+        /*
+            Sum all array 
+        */
+        export function sum(arr){
+            return arr.reduce(function(acc,value){
+                return acc + value
+            },0);
+        }
+
+        /* 
+            FUNCTION GET PROP OBJECT
+        */
+        export function prop(key){
+            return function(obj){
+                return obj[key];
+            }
+        }
+        
+        /* 
+            FUNÇÃO MULTIPLICADOR DINAMICO
+            DINAMIC MULTIPLY FUNCTION
+        */
+        export function multiply(multi)
+        {
+            return function(number){
+                return number * multi;
             }
         }
